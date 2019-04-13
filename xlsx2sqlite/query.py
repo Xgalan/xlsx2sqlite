f3f325e6738efc0e7a0c5623fd06cdb6978f35c0
Xgalan <erik_mascheri@fastmail.com>
2019-04-13 20:49:38 +0200

    .

--- xlsx2sqlite/query.py
+++ xlsx2sqlite/query.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import sqlite3
 
 
@@ -13,6 +11,14 @@ class DatabaseWrapper:
                    specified the class will instantiate a sqlite3
                    in memory database.
     """
+    SQL_QUERY = {
+        'create_table': 'CREATE TABLE IF NOT EXISTS {name} ({definitions});',
+        'create_view': 'CREATE VIEW IF NOT EXISTS {name} AS {select};',
+        'drop_entity': 'DROP {entity} IF EXISTS {name};',
+        'insert_into': 'INSERT INTO {tablename}({fields}) VALUES ({args});',
+        'replace': """REPLACE INTO {tablename}({fields}) VALUES ({args});""",
+        }
+
     def __init__(self, path=None):
         if path is None:
             self._db = sqlite3.connect(':memory:')
@@ -57,11 +63,10 @@ class DatabaseWrapper:
         :key definitions: Definitions as accepted by the Sqlite3 SQL
                           query dialect.
         """
-        sql_query = 'CREATE TABLE IF NOT EXISTS {name} ({definitions});'
         parameters = {'name': tablename, 'definitions': definitions}
         messages = {'error': 'Error when creating table: ',
                     'success': 'Created table: ' + parameters['name']}
-        self._execute(sql_query, parameters, messages)
+        self._execute(self.SQL_QUERY['create_table'], parameters, messages)
 
     def create_view(self, viewname=None, select=None):
         """The CREATE VIEW command assigns a name to a pre-packaged SELECT
@@ -78,11 +83,10 @@ class DatabaseWrapper:
         :key select: `SELECT` SQL query to use as argument in the
                      `CREATE VIEW IF NOT EXISTS` statement.
         """
-        sql_query = 'CREATE VIEW IF NOT EXISTS {name} AS {select};'
         parameters = {'name': viewname, 'select':  select}
         messages = {'error': 'Error when creating view: ',
                     'success': 'Created view: ' + parameters['name']}
-        self._execute(sql_query, parameters, messages)
+        self._execute(self.SQL_QUERY['create_view'], parameters, messages)
 
     def drop_entity(self, entity_name=None, entity_type=None):
         """Drop the specified entity from the database.
@@ -102,13 +106,12 @@ class DatabaseWrapper:
         :key entity_name: Name of the entity to drop.
         :key entity_type: Type of the entity to drop.
         """
-        sql_query = 'DROP {entity} IF EXISTS {name};'
         parameters = {'entity': entity_type,
                       'name': entity_name}
         messages = {'error': 'Error when deleting ' + entity_type + ' ' + entity_name,
                     'success': 'Deleted ' + entity_type + ' ' + entity_name}
         if entity_type in ['TABLE', 'VIEW']:
-            self._execute(sql_query, parameters, messages)
+            self._execute(self.SQL_QUERY['drop_entity'], parameters, messages)
 
     def _executemany(self, query, parameters, data):
         try:
@@ -126,9 +129,8 @@ class DatabaseWrapper:
         :key data: List of values to be passed as arguments
                    to the SQL statement.
         """
-        sql_query = 'INSERT INTO {tablename}({fields}) VALUES ({args});'
         parameters = {'tablename': tablename, 'fields': fields, 'args': args}
-        self._executemany(sql_query, parameters, data)
+        self._executemany(self.SQL_QUERY['insert_into'], parameters, data)
 
     def insert_or_replace(self, tablename=None, fields=None, args=None,
                           data=None):
@@ -140,9 +142,8 @@ class DatabaseWrapper:
         :key data: List of values to be passed as arguments
                    to the SQL statement.
         """
-        sql_query = """REPLACE INTO {tablename}({fields}) VALUES ({args});"""
         parameters = {'tablename': tablename, 'fields': fields, 'args': args}
-        self._executemany(sql_query, parameters, data)
+        self._executemany(self.SQL_QUERY['replace'], parameters, data)
 
     def select(self, columns=None, from_table=None, where=None):
         """Executes a `SELECT` query on the database.
