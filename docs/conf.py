a7f4b19680ec8e591d0da0f6e02b57b79d8787e3
Xgalan <erik.mascheri@gmail.com>
2021-03-27 15:11:23 +0100

    .

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
-    'sphinx_click.ext',
+    'sphinx_click',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
