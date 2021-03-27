754f8cad729933f29e0a6eb80a7dff44bbf351ce
Xgalan <erik.mascheri@gmail.com>
2021-03-27 15:00:01 +0100

    Signed-off-by: Xgalan <erik.mascheri@gmail.com>

--- xlsx2sqlite/__init__.py
+++ xlsx2sqlite/__init__.py
@@ -1,5 +1,5 @@
 """xlsx2sqlite - Generate a Sqlite3 database from a Office Open XML file."""
 
-__version__ = '0.1.1'
-__author__ = 'Erik Mascheri <erik_mascheri@fastmail.com>'
+__version__ = '0.1.2'
+__author__ = 'Erik Mascheri <erik.mascheri@gmail.com>'
 __all__ = []
