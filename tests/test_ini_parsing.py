01fddc2bb9f4cea4b66f108b63647f288e701ceb
Xgalan <erik.mascheri@gmail.com>
2021-12-05 20:13:17 +0100

    rewrited some tests

--- tests/test_ini_parsing.py
+++ tests/test_ini_parsing.py
@@ -14,13 +14,13 @@ def ini_path():
 
 @pytest.fixture
 def mandatory_sections():
-    return set(['PATHS', 'WORKSHEETS'])
+    return set(['PATHS',])
 
 
 @pytest.fixture
 def model_keywords():
     return [
-        'table_name',
+        'db_table',
         'columns',
         'primary_key',
         'unique',
@@ -45,43 +45,46 @@ def test_mandatory_sections(config, mandatory_sections):
 def test_worksheets_as_sections(config):
     """Test if the listed worksheets to import are declared as sections.
     """
-    names = config.get_imports()['worksheets']
+    names = config.get_tables_names
     assert any(names)
-    for name in names:
-        s = config.get(name)
-        assert s['worksheet'] == name
+    assert names == set(config.get_models.keys())
 
 
-def test_duplicate_keys(config):
+def test_duplicate_keys(config, model_keywords):
     """Is it a problem working with duplicate keys in different sections of the ini file?
     Yes, it is... if using config.get method
     Use _parser.get, can use nested items
     """
-    worksheets_names = config.get_imports()['worksheets']
+    worksheets_names = config.get_tables_names
     assert any(worksheets_names)
+
     names = [
         config._parser.get(name, 'worksheet', fallback=None) for name in worksheets_names
     ]
-    assert names == worksheets_names
+
     models = {
         name: config.get(name) for name in worksheets_names
     }
-    assert [k for k in models.keys()] == worksheets_names
+    assert [k for k in models.keys()] == list(worksheets_names)
+    #TODO: assure that no model keywords is outside of a section as a key
 
 
 def test_get_model(config):
-    worksheets_names = config.get_imports()['worksheets']
+    worksheets_names = config.get_tables_names
     assert any(worksheets_names)
-    m = config.get_model('SalesOrders')
-    assert m['SalesOrders']['table_name'] == ['Sales Orders']
+    m = config.get_models
+    assert m['SalesOrders']['db_table'] == ['Sales Orders']
     cols = m['SalesOrders']['columns']
     assert type(cols) == list
 
 
-def test_section_complex_name(config):
+def test_section_complex_name(config, mandatory_sections):
     sections = config.sections()
-    pass
+    worksheets_names = config.get_tables_names
+    assert set(sections) - mandatory_sections == worksheets_names
+    assert 'Complex UTF-8 key value àèò§' in worksheets_names
 
 
 def test_custom_header_row(config):
+    #TODO
     pass
