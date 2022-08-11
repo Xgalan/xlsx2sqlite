9efbb0bb5d6c1e5b1215991aba89526ba0834cea
Xgalan <erik.mascheri@gmail.com>
2022-08-11 22:12:39 +0200

    added test fixture to "cleanup" test files

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -14,15 +14,23 @@ def new_controller_diskdb(disk_db_ini):
     return controller.new_controller(disk_db_ini)
 
 
+def delete_db_file():
+    from pathlib import Path
+
+    samples_path = Path("./tests/samples/")
+    if samples_path / "test_db.sqlite":
+        f = samples_path / "test_db.sqlite"
+        f.unlink()
+
+
+@pytest.fixture
+def db_cleanup():
+    yield
+    # This is executed when the test using the fixture is done
+    delete_db_file()
+
+
 class TestController:
-    def clean_before_tests(self):
-        from pathlib import Path
-
-        samples_path = Path("./tests/samples/")
-        if samples_path / "test_db.sqlite":
-            f = samples_path / "test_db.sqlite"
-            f.unlink()
-
     def test_new_controller(self, new_controller):
         """support for in-memory database"""
         self.ctrler = new_controller
@@ -32,9 +40,8 @@ class TestController:
         assert self.ctrler._ini.get("root_path") == "./tests/samples"
         assert self.ctrler.get_db_table_name("SalesOrders") == "Sales Orders"
 
-    def test_initialize_db(self, new_controller_diskdb):
+    def test_initialize_db(self, new_controller_diskdb, db_cleanup):
         """unit testing initialize_db method"""
-        self.clean_before_tests()
         self.ctrler = new_controller_diskdb
         self.ctrler.initialize_db()
         self.ctrler = new_controller_diskdb
@@ -47,8 +54,7 @@ class TestController:
         )
         assert set([t[1] for t in res]) == tables
 
-    def test_drop_tables(self, new_controller_diskdb):
-        self.clean_before_tests()
+    def test_drop_tables(self, new_controller_diskdb, db_cleanup):
         self.ctrler = new_controller_diskdb
         self.ctrler.initialize_db()
         self.ctrler = new_controller_diskdb
